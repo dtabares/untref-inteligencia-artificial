575630ee737449cf544572ee194c8843fba58b4d
Diego Tabares <tabares@ar.ibm.com>
2018-06-20 17:19:25 -0300

    v1

--- tp4/reinforcement_learning.py
+++ tp4/reinforcement_learning.py
@@ -5,12 +5,14 @@ import random
 import numpy as np
 from move_memory import MoveMememory
 from time import sleep
+import matplotlib.pyplot as plt
+
 
 #Allowed moves in the board
 moves = ['UP','LEFT','RIGHT','DOWN']
 oposite_moves = {'UP': 'DOWN', 'DOWN': 'UP', 'LEFT': 'RIGHT','RIGHT': 'LEFT'}
 #Number of iterations for training
-iterations = 2
+iterations = 50000
 #Drawing stuff
 rectangles = []
 rectangle_h = 50
@@ -25,12 +27,18 @@ rewards = []
 #Training stuff
 number_of_moves = 0
 score = 0
+best_score = 0
+scores = []
 start_position = 0
 end_position = grid_size_x - 1
 end_flag = False
 q_table = np.zeros((grid_size_x * grid_size_y, len(moves), 1))
-learning_rate = 0.9
-decay = 0.001
+move_type = []
+#learning rate
+alpha = 0.6
+decay = 0.000001
+eps = 0.9
+gamma = 0.9
 
 #Needed for world moves and actions
 fringe_left_values = []
@@ -64,6 +72,9 @@ def reset_canvas():
 
 def random_move():
   intended_direction = random.choice(moves)
+  return actual_move(intended_direction)
+
+def actual_move(intended_direction):
   move_list, probabilities = probabilities_for_a_move(intended_direction)
   actual_direction = np.random.choice(move_list,1,p=probabilities)
   return actual_direction
@@ -100,34 +111,62 @@ def calculate_new_pos_and_reward(actual_pos,direction):
   return new_pos, rewards[new_pos]
 
 def move(actual_pos):
-  direction = random_move()
-  print("direction: ", direction)
+  if np.random.uniform(0, 1) < eps:
+    direction = random_move()
+    #print ("move: random")
+    move_type.append(0)
+  else:
+    best_move_idx = np.argmax(q_table[actual_pos])
+    best_move = moves[best_move_idx]
+    direction = actual_move(best_move)
+    #print ("move: best")
+    move_type.append(1)
+
+  #print("direction: ", direction)
   new_pos, reward = calculate_new_pos_and_reward(actual_pos,direction)
-  global score, end_flag
-  score = score + reward
+  global end_flag
   global number_of_moves
   number_of_moves = number_of_moves + 1
   if (new_pos == end_position):
     end_flag = True
-  return new_pos
+  return new_pos, reward, direction
   
 
 def q_learning():
+  print ('using Q-Learning')
   for iter in range(iterations):
-    global score, end_flag, number_of_moves
+    print("iteration #:", iter)
+    global score, end_flag, number_of_moves,decay, best_score,eps, gamma
+    print("gamma: ", gamma)
     score = 0
     end_flag = False
     number_of_moves = 0
-    pos = start_position
+    actual_position = start_position
     while (end_flag == False):
-      draw_x(pos)
+      draw_x(actual_position)
       root.update()
-      print("iteration #:", iter)
-      print("pos: ", pos)
-      print("score:", score)
-      print("number_of_moves",number_of_moves)
-      pos = move(pos)
-      sleep(0.01)
+      #print("iteration #:", iter)
+      #print("actual_position: ", actual_position)
+      #print("score:", score)
+      #print("number_of_moves",number_of_moves)
+      #print("eps: ", eps)
+      new_position, reward, action = move(actual_position)
+      #update q_table
+      q_table[actual_position,moves.index(action)] = (1 - alpha) * q_table[actual_position,moves.index(action)] + (alpha * (reward + (gamma * np.max(q_table[new_position]))))
+      score = score + reward
+      actual_position = new_position
+      eps -= decay
+      #sleep(0.01)
+    scores.append(score)
+    #gamma -= decay
+    if (score > best_score or iter == 1):
+      best_score = score
+    #print("round score: ", score)
+    #print("best score: ", best_score)
+    #print("------------------------")
+    #sleep(3)
+  print("Best Score: ", best_score)
+  print("eps: ", eps)
 
 # Define la ventana principal de la aplicación
 root = Tk()
@@ -162,4 +201,18 @@ for x in rewards:
   print(x)
 canvas.pack(fill=BOTH, expand=1)
 q_learning()
-root.mainloop()
+#root.mainloop()
+
+plt.plot(scores)
+plt.ylabel('scores')
+plt.xlabel('round')
+plt.show()
+
+plt.plot(move_type,'ro')
+plt.ylabel('move type -- 0 = random, 1 = best')
+plt.xlabel('moves')
+plt.show()
+
+np.set_printoptions(precision=3)
+np.set_printoptions(suppress=True)
+print(q_table)
